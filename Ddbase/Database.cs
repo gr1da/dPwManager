77012a8dd6c610fa514c53c066f3ffe3c4abc832
gr1da <62083098+gr1da@users.noreply.github.com>
2023-01-23 20:44:53 -0700

    Initial commit

---
+++ Ddbase/Database.cs
@@ -0,0 +1,42 @@
+﻿namespace Ddbase;
+
+public partial class Database
+{
+    string Filename { get; }
+
+    FileStream _file;
+
+    public Database(string filename) {
+        this.Filename = filename;
+        //try {
+            //we expect file to already exist, to avoid situations where an
+            //existing file is overwritten. Use DatabaseMaker
+            _file = new FileStream(
+                Filename,
+                FileMode.Open, 
+                FileAccess.ReadWrite,
+                FileShare.None);
+        //}
+        //catch (Exception e){
+        //    throw e;
+        //}
+    }
+
+    ~Database() {
+        _file.Dispose();
+    }
+}
+
+public partial class DatabaseMaker
+{
+    string Path { get; set; }
+
+    DatabaseMaker(string path) {
+        this.Path = path;
+    }
+
+    public Database build() {
+        File.Create(Path);
+        return new Database(Path);
+    }
+}
---
+++ DdbaseTest/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace DdbaseTest;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
