67d44998925ff7e91a6e037116796bd4deaf6afc
gr1da <62083098+gr1da@users.noreply.github.com>
2023-01-25 15:50:22 -0700

    Sync

--- Ddbase/Database.cs
+++ Ddbase/Database.cs
@@ -1,42 +1,62 @@
-﻿
-namespace Ddbase;
-
-public interface IBackingStore : IDisposable
-{
-    public bool Append(string key, string data);
-    public string[] Load();
-    public bool RecenterAt(string key);
-}
-
-public partial class Database
-{
-    IBackingStore _backing;
-    IDictionary<string, string> _memStore;
-
-    public Database(IBackingStore backend, 
-                    IDictionary<string, string> memStore) 
-    {
-        this._backing = backend;
-        this._memStore = memStore;
+﻿namespace Ddbase;
+public class Database {
+    public struct VersionedValue {
+        public readonly int Version;
+        public readonly string Value;
+    }
+    public interface IBackingStore : IDisposable {
+        public int LoadSize { get => 0; set => value = 0; }
+        public bool Append(string key, VersionedValue data);
+        public KeyValuePair<string, List<VersionedValue>>[] Load();
+        public bool ReloadAt(string key) => true;
+        public void Cleanup(int versToKeep) { }
     }
 
+    IBackingStore _backing;
+    IDictionary<string, List<VersionedValue>> _memStore;
+
+    public int CachedSize { get => _memStore.Count; }
+
+    public Database(IBackingStore backend, 
+                    IDictionary<string, List<VersionedValue>> memStore) {
+
+        _backing = backend;
+        _memStore = memStore;
+    }
     ~Database() {
         _backing.Dispose();
     }
-
-    public bool read(string key, out string value) {
-        value = "";
-        return true;
+    private void loadFromBacking() {
+        foreach (var kv in _backing.Load()) {
+            if (! _memStore.ContainsKey(kv.Key)) {
+                _memStore[kv.Key] = kv.Value;
+            }
+        }
     }
-
-    public bool update(string key, string value) {
-        return true;
+    private List<VersionedValue>? getAllVals(string key) {
+        if (!_memStore.ContainsKey(key)) {
+            //then try recaching
+            _backing.ReloadAt(key); 
+            loadFromBacking();
+            if (!_memStore.ContainsKey(key)) {
+                return null;
+            }
+        }
+        return _memStore[key];
     }
-
-    public bool delete(string key) {
-        return true;
+    public string? getOfVersion(string key, int version) {
+        List<VersionedValue>? allVals = getAllVals(key);
+        if (allVals is null) {
+            return null;
+        }
+        return allVals.Find(v => v.Version == version).Value;
     }
+    public bool getNewest(string key) {
 
+    }
+    public string this[string key] {
+        get => "";
+    } 
 }
 
     /* public static Database make(string filename) {
