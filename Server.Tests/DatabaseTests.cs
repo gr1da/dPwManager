e1f5e5b4e4132ca6e95dbd78d66f95446b4a2a02
gr1da <62083098+gr1da@users.noreply.github.com>
2023-01-25 22:34:08 -0700

    Redo

---
+++ Server.Tests/DatabaseTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using dPwManager.Server;
+
+namespace Server.Tests;
+
+[TestClass]
+public class DatabaseTest 
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
---
+++ Server/Database.cs
@@ -0,0 +1,44 @@
+﻿namespace dPwManager.Server;
+
+using KvPair = KeyValuePair<string, string>;
+
+//Interface to database backing store, like a file. Implements IDisposable so
+//that file handles can be closed by destructor in Database class.
+public interface IBackingStore : IDisposable {
+    //Return up to "blocks" number of data, or as many as are available.
+    public KvPair[] Read(int blocks);
+    public KvPair[] ReadAll();
+    //Write to end of backing store (creation AND update)
+    public void Append(KvPair data);
+    public void Delete(string key);
+    //Cleans up old data that has been deleted or updated. Needed since the only
+    //mutation done on backing store is appending. Deletions or updates don't
+    //actually change data until this is called.
+    public void Clean();
+}
+
+public partial class Database {
+    private IBackingStore _backing;
+    //Dictionary of key/value pairs where key is string, value is a collection
+    //of (int, string) tuples holding multiple versions of data associated with
+    //a key.
+    private IDictionary<string, ICollection<(int, string)>> _data;
+
+    //Constructor intended to be called by derived class that implements a
+    //factory
+    protected Database(IBackingStore backingStore,
+        IDictionary<string, ICollection<(int, string)>> dataStructure) {
+        
+        _backing = backingStore;
+        _data = dataStructure;
+    }
+}
+
+public partial class DummyBackingStore : IBackingStore {
+    public KvPair[] Read(int blocks) => ReadAll();
+    public KvPair[] ReadAll() => new KvPair[] {new KvPair("dummy", "store")};
+    public void Append(KvPair data) { }
+    public void Delete(string key) { }
+    public void Clean() { }
+    public void Dispose() { }
+}
